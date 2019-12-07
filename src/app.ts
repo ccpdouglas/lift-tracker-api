e90b39159d718d9c67c52426fe2a4d1f7cee1ebb
Callum Douglas <callumcpdouglas@gmail.com>
2019-12-07 17:24:47 +0000

    update exercise route and use body parser

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import exerciseRouter from "./api/routes/exercise"
+import bodyParser from "body-parser"
 
 const app = express()
 
-app.use("/exercise", exerciseRouter)
+app.use(bodyParser.json())
+app.use("/exercises", exerciseRouter)
 
 export default app
