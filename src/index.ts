ada97c7a18837fd46efe87f287b3f40ee9f238b4
Callum Douglas <callumcpdouglas@gmail.com>
2019-12-07 17:24:58 +0000

    add mongo db connection in mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import mongoose from "mongoose"
 import app from "./app"
+
+mongoose.connect("mongodb://localhost:27017/liftTracker", { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.listen(3000)
