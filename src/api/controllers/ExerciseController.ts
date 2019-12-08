bf9f32cd0c096b5250603d097b4bb37565476a5c
Callum Douglas <callumcpdouglas@gmail.com>
2019-12-08 09:30:07 +0000

    add exercise controller

---
+++ src/api/controllers/ExerciseController.ts
@@ -0,0 +1,28 @@
+import { Model } from "mongoose"
+import { IExerciseController, IExercise } from "../../types"
+
+export default class ExerciseController implements IExerciseController {
+    constructor(private _ExerciseModel: Model<IExercise, {}>) {}
+
+    public async create(name: string): Promise<IExercise> {
+        const newExercise = new this._ExerciseModel({ name })
+        const document = await newExercise.save()
+        return document
+    }
+
+    public async delete(id: string): Promise<void> {
+        await this._ExerciseModel.findByIdAndDelete(id).exec()
+    }
+
+    public async get(id: string): Promise<IExercise> {
+        const document = await this._ExerciseModel.findById(id).exec()
+        return document.toJSON()
+    }
+
+    public async update(id: string, name: string): Promise<IExercise> {
+        const document = await this._ExerciseModel.findById(id, { name }).exec()
+        document.name = name
+        const updatedDocument = await document.save()
+        return updatedDocument
+    }
+}
--- src/api/routes/exercise.ts
+++ src/api/routes/exercise.ts
@@ -2,26 +2,35 @@ import { Router } from "express"
 import setRouter from "./set"
 import reportRouter from "./report"
 import Exercise from "../../models/Exercise"
+import ExerciseController from "../controllers/ExerciseController"
 
 const exerciseRouter = Router()
+const exerciseController = new ExerciseController(Exercise)
 
 exerciseRouter.get("/", async (req, res, next) => {
     const results = await Exercise.find({}).exec()
-    res.json(results)
+    return res.json(results)
 })
 
 exerciseRouter.post("/", async (req, res, next) => {
-    const { body } = req
-    const newExercise = new Exercise(body)
-    const document = await newExercise.save()
-    res.json(document)
+    const document = await exerciseController.create(req.body.name)
+    return res.json(document)
 })
 
-exerciseRouter.get("/:id")
+exerciseRouter.get("/:id", async (req, res, next) => {
+    const document = await exerciseController.get(req.params.id)
+    return res.json(document)
+})
 
-exerciseRouter.patch("/:id")
+exerciseRouter.patch("/:id", async (req, res, next) => {
+    const document = await exerciseController.update(req.params.id, req.body.name)
+    return res.json(document)
+})
 
-exerciseRouter.delete("/:id")
+exerciseRouter.delete("/:id", async (req, res, next) => {
+    await exerciseController.delete(req.params.id)
+    return res.status(200)
+})
 
 exerciseRouter.use("/set", setRouter)
 
